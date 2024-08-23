65e1faf76d8e5cbafa6c06c4eb0ddcec92a79382
D-4 <piyush.singh1315@gmail.com>
2024-08-24 00:30:13 +0530

    completed :  1. Total Sales Over Time, 2. Sales Growth Rate Over Time

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,4 +1,6 @@
 import shopifyOrders from "../schema/orderSchema.js";
+
+//the names are self explanatory
 export const totSalesDaily = async (req, res) => {
     try {
         const sales = await shopifyOrders.aggregate([
@@ -28,20 +30,145 @@ export const totSalesDaily = async (req, res) => {
     }
 };
 export const totSalesMonthly = async(req,res) => {
-    //the code below is just to check whether the DB is replying or not
     try {
-        const orders = await shopifyOrders.find({});
-        res.json(orders);
+        const sales = await shopifyOrders.aggregate([
+            {
+                $addFields: {
+                    convertedDate: { $toDate: "$created_at" },
+                    totalPriceNumber: { $toDouble: "$total_price_set.shop_money.amount" }
+                }
+            },
+            {
+                $group: {
+                    _id: { 
+                        // day: { $dayOfMonth: "$convertedDate" }, 
+                        month: { $month: "$convertedDate" }, 
+                        year: { $year: "$convertedDate" } 
+                    },
+                    totalSales: { $sum: "$totalPriceNumber" }
+                }
+            },
+            { $sort: { '_id.year': 1, '_id.month': 1 } }
+        ]);
+
+        res.json(sales);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
 }
 
-export const totSalesQuartely = async(req,res) => {
-    res.send("Quarter");
-}
+export const totSalesQuartely = async (req, res) => {
+    try {
+        const sales = await shopifyOrders.aggregate([
+            {
+                $addFields: {
+                    convertedDate: { $toDate: "$created_at" },
+                    totalPriceNumber: { $toDouble: "$total_price_set.shop_money.amount" }
+                }
+            },
+            {
+                $group: {
+                    _id: {
+                        year: { $year: "$convertedDate" },
+                        quarter: { 
+                            $ceil: {
+                                $divide: [{ $month: "$convertedDate" }, 3]
+                            }
+                        }
+                    },
+                    totalSales: { $sum: "$totalPriceNumber" }
+                }
+            },
+            { $sort: { '_id.year': 1, '_id.quarter': 1 } }
+        ]);
+        
+        res.json(sales);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+};
 
 export const totSalesYearly = async(req,res) => {
-    res.send("Annual");
+    try {
+        const sales = await shopifyOrders.aggregate([
+            {
+                $addFields: {
+                    convertedDate: { $toDate: "$created_at" },
+                    totalPriceNumber: { $toDouble: "$total_price_set.shop_money.amount" }
+                }
+            },
+            {
+                $group: {
+                    _id: { 
+                        // day: { $dayOfMonth: "$convertedDate" }, 
+                        // month: { $month: "$convertedDate" }, 
+                        year: { $year: "$convertedDate" } 
+                    },
+                    totalSales: { $sum: "$totalPriceNumber" }
+                }
+            },
+            { $sort: { '_id.year': 1 } }
+        ]);
+
+        res.json(sales);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+}
+
+export const orderCountYearly = async(req,res) => {
+    try{
+        const sales = await shopifyOrders.aggregate([
+            {
+                $addFields: {
+                    convertedDate: { $toDate: "$created_at" },
+                    totalQuantity: 1
+                }
+            },
+            {
+                $group: {
+                    _id: { year: { $year: "$convertedDate" } },
+                    totalQuantity: { $sum: "$totalQuantity" }
+                }
+            },
+            { $sort: { '_id.year': 1 } } //total orders per year
+        ]);
+    
+        res.json(sales);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+}
+
+export const orderCountMonthly = async(req,res) => {
+    try{
+        const sales = await shopifyOrders.aggregate([
+            {
+                $addFields: {
+                    convertedDate: { $toDate: "$created_at" },
+                    totalQuantity: 1
+                }
+            },
+            {
+                $group: {
+                    _id: { 
+                        month: { $month: "$convertedDate" } ,
+                    year: { 
+                        $year: "$convertedDate" 
+                    }
+                },
+                    totalQuantity: { $sum: "$totalQuantity" }
+                }},
+            { $sort: { '_id.year': 1, '_id.month':1 } } //total orders per year
+        ]);
+    
+        res.json(sales);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 }
--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -1,13 +1,15 @@
 import express from "express"
 const router = express.Router();
-import { totSalesDaily, totSalesMonthly, totSalesQuartely, totSalesYearly } from "../controllers/salesController.js";
+import { orderCountMonthly, orderCountYearly, totSalesDaily, totSalesMonthly, totSalesQuartely, totSalesYearly } from "../controllers/salesController.js";
 
 // router.use('/', () =>{
 //     console.log("Sales router is working");
 // })
 router.get('/total-sales-daily', totSalesDaily);
-router.use('/total-sales/monthly', totSalesMonthly);
+router.use('/total-sales-monthly', totSalesMonthly);
 router.use('/total-sales/0.25', totSalesQuartely);
-router.use('/total-sales/year', totSalesYearly);
+router.use('/total-sales-year', totSalesYearly);
+router.use('/growth-monthly', orderCountMonthly);
+router.use('/growth-yearly', orderCountYearly);
 
 export default router;
