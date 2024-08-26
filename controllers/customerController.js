669b576a75252a50797b20e3f335a16990d33e5d
D-4 <piyush.singh1315@gmail.com>
2024-08-26 17:33:45 +0530

    t6: Customer Lifetime Value by Cohorts + adjustments for chartjs

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,6 +1,17 @@
 import ShopifyCustomer from "../schema/customerSchema.js"
 import shopifyOrders from "../schema/orderSchema.js";
 
+const formatChartData = (data, labelFunc, dataFunc) => ({
+    labels: data.map(labelFunc),
+    datasets: [{
+        label: 'Data',
+        data: data.map(dataFunc),
+        backgroundColor: 'rgba(75, 192, 192, 0.2)', // Change this to your preferred color
+        borderColor: 'rgba(75, 192, 192, 1)', // Change this to your preferred color
+        borderWidth: 1
+    }]
+});
+
 export const monthlyCust = async (req, res) => {
     try {
         const customers = await ShopifyCustomer.aggregate([
@@ -82,17 +93,20 @@ export const dailyRepeatCustomers = async (req, res) => {
                     orderCount: { $sum: 1 }
                 }
             },
-            // {
-            //     $match: {
-            //         orderCount: { $gt: 1 }
-            //     }
-            // },
+            {
+                // Optional: Filter customers with more than one order per day
+                $match: {
+                    orderCount: { $gt: 1 }
+                }
+            },
             {
                 $group: {
                     _id: "$_id.date",
                     totalRepeatedCustomers: { $sum: 1 }
                 }
             },
+            
+            
             {
                 $project: {
                     _id: 0,
@@ -104,9 +118,8 @@ export const dailyRepeatCustomers = async (req, res) => {
                 $sort: { 'date': 1 }
             }
         ]);
-
-        // Send the result directly as is
-        res.json(repeatedCustomers);
+        const chartData = formatChartData(repeatedCustomers, data => data.date, data => data.totalRepeatedCustomers);
+        res.json(chartData);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
@@ -114,7 +127,7 @@ export const dailyRepeatCustomers = async (req, res) => {
 };
 
 
-export const monthlyRepeatCustomers = async(req, res) => {
+export const monthlyRepeatCustomers = async (req, res) => {
     try {
         const repeatCustomers = await shopifyOrders.aggregate([
             {
@@ -125,7 +138,6 @@ export const monthlyRepeatCustomers = async(req, res) => {
             {
                 $group: {
                     _id: {
-                        // day: { $dayOfMonth: "$convertedDate" },
                         month: { $month: "$convertedDate" },
                         year: { $year: "$convertedDate" },
                         email: "$customer.email"
@@ -138,9 +150,21 @@ export const monthlyRepeatCustomers = async(req, res) => {
                     orderCount: { $gt: 1 }
                 }
             },
-            { $sort: { '_id.year': 1, '_id.month': 1 } }
+            {
+                $group: {
+                    _id: {
+                        month: "$_id.month",
+                        year: "$_id.year"
+                    },
+                    repeatCustomerCount: { $sum: 1 }
+                }
+            },
+            {
+                $sort: { '_id.year': 1, '_id.month': 1 }
+            }
         ]);
-        res.json(repeatCustomers);
+        const chartData = formatChartData(repeatCustomers, data => `${data._id.month}-${data._id.year}`, data => data.repeatCustomerCount);
+        res.json(chartData);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
@@ -157,8 +181,6 @@ export const YearlyRepeatCustomers = async(req, res) => {
             {
                 $group: {
                     _id: {
-                        // day: { $dayOfMonth: "$convertedDate" },
-                        // month: { $month: "$convertedDate" },
                         year: { $year: "$convertedDate" },
                         email: "$customer.email"
                     },
@@ -170,9 +192,23 @@ export const YearlyRepeatCustomers = async(req, res) => {
                     orderCount: { $gt: 1 }
                 }
             },
-            { $sort: { '_id.year': 1 } }
+            {
+                $group: {
+                    _id: "$_id.year",
+                    repeatCustomerCount: { $sum: 1 }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    year: "$_id",
+                    repeatCustomerCount: 1
+                }
+            },
+            { $sort: { 'year': 1 } }
         ]);
-        res.json(repeatCustomers);
+        const chartData = formatChartData(repeatCustomers, data => `${data.year}`, data => data.repeatCustomerCount);
+        res.json(chartData);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
@@ -191,7 +227,7 @@ export const quarterlyRepeatCustomers = async(req, res) => {
                     _id: {
                         quarter: { $ceil: { $divide: [{ $month: "$convertedDate" }, 3] } },
                         year: { $year: "$convertedDate" },
-                        email: "$email"
+                        email: "$customer.email"
                     },
                     orderCount: { $sum: 1 }
                 }
@@ -201,9 +237,27 @@ export const quarterlyRepeatCustomers = async(req, res) => {
                     orderCount: { $gt: 1 }
                 }
             },
-            { $sort: { '_id.year': 1, '_id.quarter': 1 } }
+            {
+                $group: {
+                    _id: {
+                        year: "$_id.year",
+                        quarter: "$_id.quarter"
+                    },
+                    repeatCustomerCount: { $sum: 1 }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    year: "$_id.year",
+                    quarter: "$_id.quarter",
+                    repeatCustomerCount: 1
+                }
+            },
+            { $sort: { 'year': 1, 'quarter': 1 } }
         ]);
-        res.json(repeatCustomers);
+        const chartData = formatChartData(repeatCustomers, data => `Q${data.quarter} ${data.year}`, data => data.repeatCustomerCount);
+        res.json(chartData);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
@@ -233,6 +287,72 @@ export const customerGeographicalDistribution = async (req, res) => {
         ]);
 
         res.json(customers);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+export const customerLifetimeValueByCohorts = async (req, res) => {
+    try {
+        const clvByCohorts = await shopifyOrders.aggregate([
+            // Convert created_at to a date object
+            {
+                $addFields: {
+                    convertedDate: { $toDate: "$created_at" },
+                    totalPriceNumber: { $toDouble: "$total_price_set.shop_money.amount" }
+                }
+            },
+            // Group by customer ID and calculate first purchase month and year
+            {
+                $group: {
+                    _id: "$customer.id",
+                    firstPurchaseDate: { $min: "$convertedDate" },
+                    lifetimeValue: { $sum: "$totalPriceNumber" }
+                }
+            },
+            // Project the cohort and lifetime value
+            {
+                $project: {
+                    _id: 0,
+                    customerId: "$_id",
+                    cohort: { 
+                        year: { $year: "$firstPurchaseDate" },
+                        month: { $month: "$firstPurchaseDate" }
+                    },
+                    lifetimeValue: 1
+                }
+            },
+            // Group by cohort to sum the lifetime values
+            {
+                $group: {
+                    _id: "$cohort",
+                    totalLifetimeValue: { $sum: "$lifetimeValue" },
+                    numberOfCustomers: { $sum: 1 }
+                }
+            },
+            // Sort by cohort (optional)
+            {
+                $sort: { "_id.year": 1, "_id.month": 1 }
+            },
+            // Project final output
+            {
+                $project: {
+                    _id: 0,
+                    cohort: {
+                        $concat: [
+                            { $toString: "$_id.year" }, "-",
+                            { $toString: { $substr: ["$_id.month", 0, 2] } }
+                        ]
+                    },
+                    totalLifetimeValue: 1,
+                    numberOfCustomers: 1,
+                    avgLifetimeValue: { $divide: ["$totalLifetimeValue", "$numberOfCustomers"] }
+                }
+            }
+        ]);
+
+        res.json(clvByCohorts);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { YearlyRepeatCustomers, customerGeographicalDistribution, dailyRepeatCustomers, monthlyCust, monthlyRepeatCustomers, quarterlyRepeatCustomers, yearlyCust } from "../controllers/customerController.js";
+import { YearlyRepeatCustomers, customerGeographicalDistribution, customerLifetimeValueByCohorts, dailyRepeatCustomers, monthlyCust, monthlyRepeatCustomers, quarterlyRepeatCustomers, yearlyCust } from "../controllers/customerController.js";
 const router = express.Router();
 
 // router.use('/', (req,res) =>{
@@ -13,5 +13,5 @@ router.use('/repeat-customers/monthly', monthlyRepeatCustomers);
 router.use('/repeat-customers/0.25', quarterlyRepeatCustomers);
 router.use('/repeat-customers/yearly', YearlyRepeatCustomers);
 router.use('/location', customerGeographicalDistribution);
-
+router.use('/lifetime-value', customerLifetimeValueByCohorts);
 export default router;
